77c7250d6f9029738a645d616328df72051a12f1
IgorBulakh <bulach.igor@gmail.com>
2021-04-14 13:47:34 +0300

    task-1/3 fixid

--- first-task/js/task-3.js
+++ first-task/js/task-3.js
@@ -4,33 +4,6 @@ const ADMIN_PASSWORD = 'qwerty';
 let message;
 const password = prompt('Введите пароль!');
 
-// if (ADMIN_PASSWORD !== true) {
-//  prompt('');
-//   message = 'Отменено пользователем!';
-// }
-// else if (ADMIN_PASSWORD === 'jqueryismyjam') {
-//   prompt('')
-//  message = 'Добро пожаловать!';
-
-// } else {
-//   prompt('')
-//    message = 'неверный пароль!';
-// }
-
-
-// if (ADMIN_PASSWORD === 'jqueryismyjam') {
-//  prompt('');
-//   message = 'Добро пожаловать!';
-// }
-// else if (ADMIN_PASSWORD !== 'jqueryismyjam') {
-//   prompt('')
-//   message = 'неверный пароль!';
-
-// } else {
-//   prompt('')
-//    message = 'Отменено пользователем!';
-// }
-
 
 
 if (password === ADMIN_PASSWORD) {
