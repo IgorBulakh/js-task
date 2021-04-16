c17825e4f5d3d585e7557448c2b24be7585d6250
IgorBulakh <bulach.igor@gmail.com>
2021-04-16 14:54:31 +0300

    task 4/1 structurre

--- first-task/js/task-4.js
+++ first-task/js/task-4.js
@@ -1,6 +1,42 @@
 'use strict';
 
-const credit = 23580;
-const pricePerDroid = 300;
-let totalPrice = 0;
+let credit = 23580;
+let pricePerDroid = 3000;
+
+let value = prompt("введите количество Droid");
+// value = Number(value);
+
+let totalPrice = value;
+// totalPrice = pricePerDroid * value;
+let totalSumm = pricePerDroid * value;
+let totalBalance = 0;
+
+
+
+
+
+if (value === null) {
+
+  console.log('отминено пользователем')
+  
+} else if (totalSumm >= credit) {
+
+console.log( "Недостаточно средств на счету!")
+
+} 
+
+else{
+
+  value = Number(value);
+// totalPrice = pricePerDroid * value;
+  totalBalance = credit - totalSumm;
+
+console.log(`Вы купили ${value} дроидов, на счету осталось ${totalBalance} кредитов.`)
+// console.log(`Ви купили ${value} droids на суму ${totalSumm} credits`);
+// console.log(`у вас осталось доступних ${totalBalance} credits`);
+
+}
+  
+
+
 
