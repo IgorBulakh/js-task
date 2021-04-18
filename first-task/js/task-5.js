2d5c1c98cd89ab888cd644cfb26b2cd313dcc058
IgorBulakh <bulach.igor@gmail.com>
2021-04-18 19:03:07 +0300

    registr-string-task -5/1

--- first-task/js/task-5.js
+++ first-task/js/task-5.js
@@ -1,40 +1,69 @@
 'use strict';
 
 
+const countryDeliver = prompt('ввдите страну').toLowerCase();
 
 
-
-let countryDyliver = prompt('ввдите страну');
-// let normalizeInput = countryDyliver.toLowerCase();
-
-
+// const materializDeliver = countryDeliver.toLowerCase();
 
 
 let totalDeliver ;
 
-switch (countryDyliver) {
-  case 'Китай':
+
+switch (countryDeliver) {
+  
+  case 'китай':
     totalDeliver = 'deliver: 100 кредитов';
     break;
 
-  case 'Чили':
+  case 'чили':
       totalDeliver = 'deliver: 250 кредитов';
     break;
-  case 'Австралия':
+  case 'австралия':
       totalDeliver = 'deliver: 170 кредитов';
     break;
-  case 'Индия':
+  case 'индия':
       totalDeliver = 'deliver: 80 кредитов';
     break;
-  case 'Ямайка':
+  case 'ямайка':
       totalDeliver = 'deliver: 120 кредитов';
     break;
 
+    case '':
+      alert( 'ups');
+      console.log(countryDeliver);
+  break;
+
+
   default:
     alert('В вашей стране доставка не доступна');
-    totalDeliver = 'fuk you!'
-    
+    totalDeliver = 'fuk you!' 
 }
 
+
 console.log(totalDeliver);
+
+// switch (r) {
+
+  
+//   case null:
+//     totalDeliver = 'not text...';
+//     break;
+//   case 'китай':
+//     // countryDyliver = countryDyliver.toLowerCase();
+//     totalDeliver = 'deliver: 100 кредитов';
+//     break;
+
+//     case 'чили':
+//       // countryDyliver = countryDyliver.toLowerCase();
+//       totalDeliver = 'deliver: 250 кредитов';
+//     break;
+
+//   default:
+//     alert('В вашей стране доставка не доступна');
+//     totalDeliver = 'fuk you!'
+    
+// }
+
+
 
