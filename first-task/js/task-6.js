50848e8aad74a2e1aa42571a033a0f23b4435897
IgorBulakh <bulach.igor@gmail.com>
2021-04-20 17:46:20 +0300

    task-6-screept-task

--- first-task/js/task-6.js
+++ first-task/js/task-6.js
@@ -1,2 +1,28 @@
 'use strict';
 
+// 1. створити цикл, що запитує користувача ввести число через prompt;
+// 2. виход з циклу при натисканні cancel (ввод null);
+// 3.Якщо ввели не null перевести в число та добавити в total;
+// 4.по завершенню цикла вивести суміжну суму введених чисел в alert;
+
+
+let total = 0;
+
+do {
+  let input = prompt('ввсти число');
+
+  if (input === null) {
+    alert('ooo... no! you staped this cicle');
+
+    break;
+  }
+
+  input = Number(input);
+
+  total += input;
+ 
+
+  
+} while (true);
+
+console.log(total);
