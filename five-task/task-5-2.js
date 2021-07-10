dad330b84d6c99c3c117c808b8491fa7ec80b02b
IgorBulakh <bulach.igor@gmail.com>
2021-07-10 14:16:14 +0300

    add-metho-class

--- five-task/task-5-2.js
+++ five-task/task-5-2.js
@@ -9,17 +9,36 @@ followers - число
 Добавь метод getInfo(), который, выводит строку: User ${имя} is ${возраст} years old and has ${кол-во фоловеров} followers
 */
 //===================================================
+/*
 const User = function ({ name, age, followers }) {
   this.name = name;
   this.age = age;
   this.followers = followers;
 };
 
+
 User.prototype.getInfo = function () {
   console.log(
     `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
   );
 };
+*/
+//====================================================
+class User {
+  constructor({ name, age, followers }) {
+    this.name = name;
+    this.age = age;
+    this.followers = followers;
+  }
+
+  getInfo() {
+    console.log(
+      `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
+    );
+  }
+}
+
+//========================================
 
 const mango = new User({
   name: "Mango",
@@ -36,3 +55,5 @@ const poly = new User({
 });
 
 poly.getInfo(); // User Poly is 3 years old and has 17 followers
+
+//===============================================
