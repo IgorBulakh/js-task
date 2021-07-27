d5f0a9f13eb4c743013b2f3d7effaa9625769d09
IgorBulakh <bulach.igor@gmail.com>
2021-07-27 17:50:02 +0300

    task-6-10

--- six-task/task-6-10.js
+++ six-task/task-6-10.js
@@ -8,12 +8,16 @@ import users from "./users.js";
 const getSortedUniqueSkills = (users) =>
   // твой код
   users
-    .reduce((acc, all) => {
-      acc.push(...all.skills);
-      acc[all] = acc.includes(all) ? acc[all] - 1 : 1;
-      return acc;
-    }, [])
-    .sort();
+    // .reduce((acc, all) => {
+    //   acc.push(...all.skills);
+    //   acc[all.skills] = acc.includes(all.skills) ? acc[all.skills] - 1 : 1;
+    //   return acc;
+    // }, [])
+    // .sort();
+    .map((user) => user.skills)
+    .reduce((allSkills, user) => [...allSkills, ...user], [])
+    .filter((elem, index, array) => array.indexOf(elem) === index)
+    .sort((a, b) => a.localeCompare(b));
 
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
